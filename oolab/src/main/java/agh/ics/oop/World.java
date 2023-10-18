d9c8fad7f07aecd7448761c09d8d124bf38bf363
zprzemek378 <zprzemek378@gmail.com>
2023-10-18 17:28:48 +0200

    initialized new project

---
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -0,0 +1,8 @@
+package agh.ics.oop;
+
+public class World {
+    public static void main(String[] args) {
+        System.out.println("system wystartował");
+        System.out.println("system zakończył działanie");
+    }
+}
