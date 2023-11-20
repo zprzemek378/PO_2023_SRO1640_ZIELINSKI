0781c1cbe3ee127d5ac5265bd72898e874274a83
zprzemek378 <zprzemek378@gmail.com>
2023-11-20 18:47:02 +0100

    changed tests

--- oolab/src/test/java/agh/ics/oop/model/SimulationTest.java
+++ oolab/src/test/java/agh/ics/oop/model/SimulationTest.java
@@ -31,4 +31,32 @@ public class SimulationTest {
         assertEquals(expectedDirection0, animals.get(0).getAnimalDirection());
         assertEquals(expectedDirection1, animals.get(1).getAnimalDirection());
     }
+
+    @Test
+    void simulationTest2() {
+        String[] moves = new String[]{
+                "f", "f", "b", "l", "b", "r", "f", "o", "r", "f", "b", "b", "f", "b", "b", "f", "b", "b", "f", "b", "b", "f", "f", "r", "l"
+        };
+        List<MoveDirection> directions = OptionsParser.parse(moves);
+        List<Vector2d> positions = List.of(new Vector2d(2, 2), new Vector2d(3, 4), new Vector2d(0,0));
+        Simulation simulation = new Simulation(positions, directions, new RectangularMap(8,5));
+        simulation.run();
+
+        List<Animal> animals = simulation.getAnimals();
+
+        Vector2d expectedPosition0 = new Vector2d(1, 3);
+        Vector2d expectedPosition1 = new Vector2d(3, 3);
+        Vector2d expectedPosition2 = new Vector2d(5, 0);
+        MapDirection expectedDirection0 = MapDirection.WEST;
+        MapDirection expectedDirection1 = MapDirection.SOUTH;
+        MapDirection expectedDirection2 = MapDirection.NORTH;
+
+        assertEquals(expectedPosition0, animals.get(0).getAnimalPosition());
+        assertEquals(expectedPosition1, animals.get(1).getAnimalPosition());
+        assertEquals(expectedPosition2, animals.get(2).getAnimalPosition());
+        assertEquals(expectedDirection0, animals.get(0).getAnimalDirection());
+        assertEquals(expectedDirection1, animals.get(1).getAnimalDirection());
+        assertEquals(expectedDirection2, animals.get(2).getAnimalDirection());
+
+    }
 }
